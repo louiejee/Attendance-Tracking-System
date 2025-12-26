02a87f3c8165b404247a4538b23f2a67a2f7e276
louiejee <50313037+louiejee@users.noreply.github.com>
2025-12-26 11:57:41 +0800

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -65,4 +65,71 @@ def insert_attendance(idno, lastname, firstname, course, level, time_logged):
         VALUES (?, ?, ?, ?, ?, ?)
     """, (idno, lastname, firstname, course, level, time_logged))
     conn.commit()
+
     conn.close()
+
+def get_all_users_formatted():
+    """Get all users formatted for the userlist page"""
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute("SELECT idno, lastname, firstname, course, level FROM users ORDER BY lastname")
+        users = cursor.fetchall()
+        
+        # Format as list of dictionaries
+        result = []
+        for user in users:
+            result.append({
+                'idno': user[0],
+                'lastname': user[1],
+                'firstname': user[2],
+                'course': user[3],
+                'level': user[4]
+            })
+        return result
+    finally:
+        cursor.close()
+        conn.close()
+
+def delete_user(idno):
+    """Delete user from database"""
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute("DELETE FROM users WHERE idno = %s", (idno,))
+        conn.commit()
+        return cursor.rowcount > 0
+    finally:
+        cursor.close()
+        conn.close()
+
+def update_user(idno, lastname, firstname, course, level):
+    """Update user in database"""
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute("""
+            UPDATE users 
+            SET lastname = %s, firstname = %s, course = %s, level = %s 
+            WHERE idno = %s
+        """, (lastname, firstname, course, level, idno))
+        conn.commit()
+        return cursor.rowcount > 0
+    finally:
+        cursor.close()
+        conn.close()
+
+def check_user_exists(idno):
+    """Check if user already exists"""
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute("SELECT idno FROM users WHERE idno = %s", (idno,))
+        return cursor.fetchone() is not None
+    finally:
+        cursor.close()
+        conn.close()
